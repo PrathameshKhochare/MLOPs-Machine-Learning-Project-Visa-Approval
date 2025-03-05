c6a41735928d28fb0992f0ff7ce64a0192ca0b0e
PrathameshKhochare <khochareprathamesh10@gmail.com>
2025-03-05 17:46:37 +0530

    Data Ingection added

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ git add .
 git commit -m "Updated"
 
 git push origin main
+
+## Workflow:
+1. Constant (export environment variable for MONGODB_URL_KEY)
+2. Entity (config and artifact entity)
+
+## Export the environment variable
+export MONGODB_URL="mongodb+srv://<username>:<password>...."
+
+export AWS_ACCESS_KEY_ID=<AWS_ACCESS_KEY_ID>
+
+export AWS_SECRET_ACCESS_KEY=<AWS_SECRET_ACCESS_KEY>
--- demo.py
+++ demo.py
@@ -2,9 +2,21 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
 
-logging.info("Welcome to our custom log")
+import os
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+
+
+#logging.info("Welcome to our custom log")
+
+# try:
+#     a = 2/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
+
+# mongo_db_url = os.getenv('MONGODB_URL')
+# print(mongo_db_url)
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
